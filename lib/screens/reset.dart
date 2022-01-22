ac2860528c22d964d81cb5416a0bb6d94ddaac12
Mafdy Magdy <mafdymagdy.mm@gmail.com>
2022-01-22 22:26:10 +0200

    Update reset.dart

--- lib/screens/reset.dart
+++ lib/screens/reset.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
 
-import '../notificationservice.dart';
+import 'notificationservice.dart';
 
 class ResetScreen extends StatefulWidget {
   @override
