5b4067582e154fd7b3bc55dc21d0f234698b1d1b
Mafdy Magdy <mafdymagdy.mm@gmail.com>
2022-01-22 22:01:46 +0200

    Create reset.dart

---
+++ lib/screens/reset.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tz;
+
+import '../notificationservice.dart';
+
+class ResetScreen extends StatefulWidget {
+  @override
+  _ResetScreenState createState() => _ResetScreenState();
+}
+
+class _ResetScreenState extends State<ResetScreen> {
+  String _email;
+  final auth = FirebaseAuth.instance;
+
+  void initState() {
+    super.initState();
+
+    tz.initializeTimeZones();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Reset Password'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(hintText: 'Email'),
+              onChanged: (value) {
+                setState(() {
+                  _email = value.trim();
+                });
+              },
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              RaisedButton(
+                child: Text('Send Request'),
+                onPressed: () {
+                  auth.sendPasswordResetEmail(email: _email);
+                  Navigator.of(context).pop();
+
+                  WidgetsFlutterBinding.ensureInitialized();
+                  NotificationService().initNotification();
+
+                  NotificationService().showNotification(1, "Fruity&Veggie",
+                      "Rest Password has been to your mail", 10);
+                },
+                color: Theme.of(context).accentColor,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
